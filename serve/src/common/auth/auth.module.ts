e381c676416547716eb859ec5bd51ffc2116d720
zhouyuxiang@epaynfc.com <zhouyuxiang@epaynfc.com>
2019-12-12 16:54:51 +0800

    chore

--- serve/src/common/auth/auth.module.ts
+++ serve/src/common/auth/auth.module.ts
@@ -5,16 +5,17 @@ import { PassportModule } from '@nestjs/passport';
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { LocalStrategy } from './local.strategy';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
     imports: [
         MemberModule,
-        PassportModule,
+        PassportModule.register({ defaultStrategy: 'jwt' }),
         JwtModule.register({
             secret: jwtConstants.secret,
             signOptions: { expiresIn: '60s' },
         }),
     ],
-    providers: [AuthService, LocalStrategy]
+    providers: [AuthService, LocalStrategy, JwtStrategy],
 })
 export class AuthModule{}
